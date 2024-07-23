5e4cf14439293ec2f2a9427f68599b249b643908
mohamedDev69 <mohamedchettah0208@gmail.com>
2024-07-23 08:20:23 +0200

    first commit

---
+++ frontend/pages/index.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+
+onMounted(() => {
+  const data = $fetch('http://backend.test/user')
+  console.log(data)
+})
+
+
+</script>
+
+<template>
+  <div class="flex justify-center items-center">
+    <h1>AnimeFolio</h1>
+
+  </div>
+</template>
+
+<style scoped>
+
+</style>
